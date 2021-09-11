e87b4c8f9a4019d8a888d80864bc50b038847409
Thomas Marchand <me@thomas-marchand.com>
2021-09-11 19:27:25 +0200

    add home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css"
+import Home from './pages/home';
+import Discover from './pages/discover/discover';
+import Explore from './pages/explore/explore';
 
-function App() {
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/discover" component={Discover} />
+          <Route path="/explore" component={Explore} />
+          <Route path="/attack" component={Home} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
-
-export default App;
