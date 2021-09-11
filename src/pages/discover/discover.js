2312fe92a0393c94be17319aed12db36566ac9e5
Thomas Marchand <me@thomas-marchand.com>
2021-09-11 19:30:53 +0200

    add homepages

---
+++ src/pages/discover/discover.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <div className="background_home">
+    </div>
+  );
+}
+export default App;
---
+++ src/pages/explore/explore.js
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+    <div className="background_home">
+    </div>
+  );
+
+}
+export default App;
