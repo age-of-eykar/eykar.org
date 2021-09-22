1ea9b90d20970465e1f571adc41bd72097775cd2
nicolasito1411 <60229704+nicolasito1411@users.noreply.github.com>
2021-09-22 16:45:46 +0200

    Biggest part of the Discover page. WIP

--- src/pages/discover/page1/page1.js
+++ src/pages/discover/page1/page1.js
@@ -1,11 +1,49 @@
-import "../discover.css"
-import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import "../discover.css";
+
+
 
 function Page1() {
   return (
     <div className="discover page1">
-    <div className="discover punchline"></div>
-    <p className="test">ug dsqgu hduilguqhdios dhulgudqls nifkjjnikmldfhusdqo ugqibqmog uqhnikgfhnfqk!fkhn gqgnkhdl nhqfdgkloj,pkqgd  gjqdorg ojùdqpj odnqfjngdkqfdqgknf</p>
+    <div className="discover punchline">
+    <h1 className="discover bigstory Title">A great punchline</h1>
+    <p className="discover bigstory Description">Cras maximus et nibh sed efficitur. Ut fringilla mi ultricies metus pulvinar, eu tempor mi viverra. In ut euismod orci. Quisque blandit posuere sem, sit amet dapibus est viverra vel. Ut eget nisl efficitur, dignissim nunc in, aliquam dolor.
+<br></br><br></br>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam consectetur eget lorem vel facilisis. Nunc nec erat interdum nunc bibendum volutpat. Sed congue, purus et sodales dictum, lectus tortor porttitor dui, vitae tempor quam purus ut dui. Donec sit amet tempus nisi. Aenean ut est at massa lobortis 
+<br></br><br></br>
+Quisque blandit posuere sem, sit amet dapibus est viverra vel. Cras maximus et nibh sed efficitur.  In ut euismod orci. Quisque blandit posuere sem, sit amet dapibus est viverra vel. Nullam quis dolor eros. Ut eget nisl efficitur, dignissim nunc in, aliquam dolor.</p>
+    </div>
+
+
+
+    <div className="discover stuff1">
+    <h1 className="discover stuff Title">This is a title</h1>
+    <p className="discover stuff Description">Sed congue, purus et sodales dictum,<br></br> lectus tortor porttitor dui,<br></br> vitae tempor quam purus ut dui.
+    <br></br> Donec sit amet tempus nisi.<br></br> 
+    Aenean ut est at massa lobortis </p>
+   <div className="discover stuff Image1"></div>
+   </div>
+
+ 
+    <div className="discover stuff2">
+    <h1 className="discover stuff Title">This is another title</h1>
+    <p className="discover stuff Description">In ut euismod orci.<br></br>  Quisque blandit posuere sem,<br></br> sit amet dapibus est viverra vel.<br></br> Litarvan chad.  </p>
+   <div className="discover stuff Image2"></div>
+   </div>
+
+
+<div className="discover bottom_line">
+</div>
+
+
+<div className="discover bottom_line_content contener">
+<div className="discover bottom_line_content circle1"></div>
+<div className="discover bottom_line_content circle"></div>
+<div className="discover bottom_line_content circle"></div>
+<div className="discover bottom_line_content circle"></div>
+<div className="discover bottom_line_content circle"></div>
+</div>
+
     </div>
  
   );
