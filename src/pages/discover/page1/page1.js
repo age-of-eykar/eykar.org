04334a7c051b472dc7ebb928d6d94ae7c7c4e4fe
Thomas Marchand <me@thomas-marchand.com>
2021-09-25 13:14:13 +0200

    cards improvement

--- src/pages/discover/page1/page1.js
+++ src/pages/discover/page1/page1.js
@@ -1,51 +1,72 @@
 import "../discover.css";
 
+import firstIllustration from '../../../img/stuff1.png';
+import secondIllustration from '../../../img/stuff2.png';
 
 
 function Page1() {
   return (
     <div className="discover page1">
-    <div className="discover punchline">
-    <h1 className="discover bigstory Title">A great punchline</h1>
-    <p className="discover bigstory Description">Cras maximus et nibh sed efficitur. Ut fringilla mi ultricies metus pulvinar, eu tempor mi viverra. In ut euismod orci. Quisque blandit posuere sem, sit amet dapibus est viverra vel. Ut eget nisl efficitur, dignissim nunc in, aliquam dolor.
-<br></br><br></br>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam consectetur eget lorem vel facilisis. Nunc nec erat interdum nunc bibendum volutpat. Sed congue, purus et sodales dictum, lectus tortor porttitor dui, vitae tempor quam purus ut dui. Donec sit amet tempus nisi. Aenean ut est at massa lobortis 
-<br></br><br></br>
-Quisque blandit posuere sem, sit amet dapibus est viverra vel. Cras maximus et nibh sed efficitur.  In ut euismod orci. Quisque blandit posuere sem, sit amet dapibus est viverra vel. Nullam quis dolor eros. Ut eget nisl efficitur, dignissim nunc in, aliquam dolor.</p>
+
+      <div className="vertical">
+        <div className="discover bigcard">
+          <h1 >A great punchline</h1>
+          <p >
+            Cras maximus et nibh sed efficitur. Ut fringilla mi ultricies metus pulvinar,
+            eu tempor mi viverra. In ut euismod orci. Quisque blandit posuere sem, sit
+            amet dapibus est viverra vel. Ut eget nisl efficitur, dignissim nunc in,
+            aliquam dolor.
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam consectetur
+            eget lorem vel facilisis. Nunc nec erat interdum nunc bibendum volutpat.
+            Sed congue, purus et sodales dictum, lectus tortor porttitor dui, vitae
+            tempor quam purus ut dui. Donec sit amet tempus nisi. Aenean ut est at massa
+            lobortis
+            <br />
+            Quisque blandit posuere sem, sit amet dapibus est viverra vel. Cras maximus
+            et nibh sed efficitur.  In ut euismod orci. Quisque blandit posuere sem,
+            sit amet dapibus est viverra vel. Nullam quis dolor eros. Ut eget nisl
+            efficitur, dignissim nunc in, aliquam dolor.
+          </p>
+        </div>
+
+        <div className="vertical">
+          <div className="discover card">
+            <div className="vertical">
+              <h1>This is a title</h1>
+              <p>
+                Sed congue, purus et sodales dictum, lectus tortor porttitor dui,
+                vitae tempor quam purus ut dui. Donec sit amet tempus nisi.
+                Aenean ut est at massa lobortis</p>
+            </div>
+            <img className="mask1" src={firstIllustration} alt="A warrior" />
+          </div>
+
+          <div className="discover card">
+            <div className="vertical">
+              <h1>This is a title</h1>
+              <p>
+                Sed congue, purus et sodales dictum, lectus tortor porttitor dui,
+                vitae tempor quam purus ut dui. Donec sit amet tempus nisi.
+              </p>
+            </div>
+            <img className="mask2" src={secondIllustration} alt="A warrior" />
+          </div>
+        </div>
+      </div>
+
+      <div className="discover bottom_line" />
+
+      <div className="discover bottom_line_content contener">
+        <div className="discover bottom_line_content circle1"></div>
+        <div className="discover bottom_line_content circle"></div>
+        <div className="discover bottom_line_content circle"></div>
+        <div className="discover bottom_line_content circle"></div>
+        <div className="discover bottom_line_content circle"></div>
+      </div>
+
     </div>
 
-
-
-    <div className="discover stuff1">
-    <h1 className="discover stuff Title">This is a title</h1>
-    <p className="discover stuff Description">Sed congue, purus et sodales dictum,<br></br> lectus tortor porttitor dui,<br></br> vitae tempor quam purus ut dui.
-    <br></br> Donec sit amet tempus nisi.<br></br> 
-    Aenean ut est at massa lobortis </p>
-   <div className="discover stuff Image1"></div>
-   </div>
-
- 
-    <div className="discover stuff2">
-    <h1 className="discover stuff Title">This is another title</h1>
-    <p className="discover stuff Description">In ut euismod orci.<br></br>  Quisque blandit posuere sem,<br></br> sit amet dapibus est viverra vel.<br></br> Litarvan chad.  </p>
-   <div className="discover stuff Image2"></div>
-   </div>
-
-
-<div className="discover bottom_line">
-</div>
-
-
-<div className="discover bottom_line_content contener">
-<div className="discover bottom_line_content circle1"></div>
-<div className="discover bottom_line_content circle"></div>
-<div className="discover bottom_line_content circle"></div>
-<div className="discover bottom_line_content circle"></div>
-<div className="discover bottom_line_content circle"></div>
-</div>
-
-    </div>
- 
   );
 }
 export default Page1;
