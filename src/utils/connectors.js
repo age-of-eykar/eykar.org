51d89e4443797f415b11f4fa7cc96853d5ffdb77
Thomas Marchand <thomas.marchand@tuta.io>
2021-11-16 19:36:39 +0100

    update networks

--- src/utils/connectors.js
+++ src/utils/connectors.js
@@ -4,14 +4,15 @@ import { LedgerConnector } from '@web3-react/ledger-connector'
 
 const POLLING_INTERVAL = 12000
 const RPC_URLS = {
-  25: 'https://evm-cronos.crypto.org',
+  //25: 'https://evm-cronos.crypto.org',
+  338: 'https://cronos-testnet-3.crypto.org:8545',
 }
 
-export const injected = new InjectedConnector({ supportedChainIds: [25] })
+export const injected = new InjectedConnector({ supportedChainIds: [338] })
 
 export const network = new NetworkConnector({
-  urls: { 25: RPC_URLS[25] },
-  defaultChainId: 25
+  urls: { 338: RPC_URLS[338] },
+  defaultChainId: 338
 })
 
-export const ledger = new LedgerConnector({ chainId: 25, url: RPC_URLS[25], pollingInterval: POLLING_INTERVAL })
+export const ledger = new LedgerConnector({ chainId: 338, url: RPC_URLS[338], pollingInterval: POLLING_INTERVAL })
