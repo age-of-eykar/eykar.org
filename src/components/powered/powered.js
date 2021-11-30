4aadc611bd28caa227ec7ace71bab0a08e9413d2
Thomas Marchand <thomas.marchand@tuta.io>
2021-11-30 23:13:29 +0100

    fix powered by cronos logo

--- src/components/powered/powered.js
+++ src/components/powered/powered.js
@@ -7,7 +7,7 @@ import logo from "../../img/cronos_logo.svg";
 function Powered() {
     return (
         <div className="powered setup">
-            <a>
+            <a className="powered layout" href="https://cronos.crypto.org/" target="https://cronos.crypto.org/" >
                 <img className="powered logo" src={logo} alt="Cronos Logo" />
                 <span className="powered text" >Powered by cronos</span>
             </a>
