763d248e4b2e35edd3b49e175d801ac7daef98b8
Thomas Marchand <thomas.marchand@tuta.io>
2022-03-02 21:26:00 +0100

    add starknet-react

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import { StarknetProvider } from '@starknet-react/core'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <StarknetProvider><Component {...pageProps} /></StarknetProvider>
 }
 
 export default MyApp
