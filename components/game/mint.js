4394d242993f55e223fbf5d2cb61bc84b2a4ad7a
Thomas Marchand <thomas.marchand@tuta.io>
2022-03-19 17:45:29 +0100

    make buttons dynamics

--- components/game/mint.js
+++ components/game/mint.js
@@ -5,6 +5,7 @@ import TextInput from '../input'
 import { useEykarContract } from '../../hooks/eykar'
 import { Spinner } from "../spinner"
 import { useStarknetTransactionManager } from '@starknet-react/core'
+import debounce from "debounce";
 
 export default function Mint({ setPage }) {
     const [name, setName] = useState("")
@@ -23,6 +24,8 @@ export default function Mint({ setPage }) {
             }
     }, [data, transactions])
 
+    const [isHoveringInfo, setIsHoveringInfo] = useState(false);
+
     return (
         <div className={styles.box}>
 
@@ -37,9 +40,11 @@ export default function Mint({ setPage }) {
 
                     <div className={styles.box_footer}>
 
-                        <div className={[styles.footer_element, styles.alert].join(" ")} >
+                        <div className={[styles.footer_element, styles.alert].join(" ")}
+                            onMouseOver={() => { setIsHoveringInfo(true) }}
+                            onMouseOut={() => setIsHoveringInfo(false)} >
                             <svg className={styles.alert_icon} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clipRule="evenodd"></path></svg>
-                            <p className={styles.button_text}>4</p>
+                            <p className={styles.button_text}>{isHoveringInfo ? "0.004 ETH" : "4"}</p>
                         </div>
 
                         {name ? <button className={[styles.footer_element, styles.button].join(" ")} onClick={() => {
@@ -68,9 +73,18 @@ export default function Mint({ setPage }) {
                                 <p className={styles.loading_text}>signing</p>
                             </div>
                             :
-                            <div className={[styles.footer_element, styles.loading_fg].join(" ")} >
-                                <Spinner color={"#7fd6aa"} className={styles.loading_icon} />
-                                <p className={styles.loading_text}>minting</p>
+                            <div className={[styles.footer_element, styles.loading_fg].join(" ")}
+                                onMouseOver={() => {
+                                    setIsHoveringInfo(true)
+                                }}
+                                onMouseOut={() => setIsHoveringInfo(false)} >
+                                {isHoveringInfo ?
+                                    <><svg className={styles.alert_icon} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+                                        <a className={styles.button_text} href={"https://goerli.voyager.online/tx/" + data} target="_blank" rel="noreferrer">show on voyager</a></>
+                                    :
+                                    <><Spinner color={"#7fd6aa"} className={styles.loading_icon} />
+                                        <p className={styles.loading_text}>minting</p></>
+                                }
                             </div>
                         }
                     </div>
