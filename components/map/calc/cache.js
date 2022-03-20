c7ea1da12ca29b1d8d49b1500445085d1588a9f5
Thomas Marchand <thomas.marchand@tuta.io>
2022-03-20 19:48:59 +0100

    tweak chunk size

--- components/map/calc/cache.js
+++ components/map/calc/cache.js
@@ -2,7 +2,7 @@ import { szudzik } from "../../../utils/deterministic";
 
 export class ChunksCache {
 
-    static halfsize = 32;
+    static halfsize = 48;
     static sideSize = 2 * ChunksCache.halfsize + 1;
 
     constructor(capacity, webgl) {
