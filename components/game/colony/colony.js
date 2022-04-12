8573d8c3b8d073a751794d2103f76e3908c0ccb8
Thomas Marchand <thomas.marchand@tuta.io>
2022-04-12 17:40:48 +0200

    add icons

--- components/game/colony/colony.js
+++ components/game/colony/colony.js
@@ -3,6 +3,8 @@ import { useEykarContract } from '../../../hooks/eykar'
 import { useStarknetCall } from '@starknet-react/core'
 import { feltToString, toNegativeNumber } from '../../../utils/felt'
 import { useColonyColor, weightedW3C } from '../../../hooks/colors'
+import WarehousesInfo from './icons/warehouses'
+import TroopsInfo  from './icons/troops'
 
 function Colony({ id }) {
 
@@ -16,7 +18,6 @@ function Colony({ id }) {
     let plots = "...";
     let [r, g, b] = useColonyColor(id);
     [r, g, b] = [r * 255, g * 255, b * 255];
-    console.log(weightedW3C(r, g, b));
     if (!loading && data) {
         name = feltToString(data.colony.name)
         x = toNegativeNumber(data.colony.x).toString()
@@ -30,8 +31,8 @@ function Colony({ id }) {
             <div>Place of Power: ({x}, {y})</div>
             <div>Territories: {plots}</div>
             <div className={styles.footer} >
-                <svg className={styles.icon} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 1.944A11.954 11.954 0 012.166 5C2.056 5.649 2 6.319 2 7c0 5.225 3.34 9.67 8 11.317C14.66 16.67 18 12.225 18 7c0-.682-.057-1.35-.166-2.001A11.954 11.954 0 0110 1.944zM11 14a1 1 0 11-2 0 1 1 0 012 0zm0-7a1 1 0 10-2 0v3a1 1 0 102 0V7z" clip-rule="evenodd"></path></svg>
-                <svg className={styles.icon} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
+                <TroopsInfo color={[r, g, b]} />
+                <WarehousesInfo color={[r, g, b]} />
             </div>
         </div>
     );
