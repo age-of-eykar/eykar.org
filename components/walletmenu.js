4b9863bec64d85ee378a89476300388dc1c7bda5
Thomas Marchand <thomas.marchand@tuta.io>
2022-04-21 16:39:34 +0200

    update starknet wallet css

--- components/walletmenu.js
+++ components/walletmenu.js
@@ -12,9 +12,9 @@ function WalletMenu({ close }) {
                 Eykar is a decentralized game built on StarkNet.
                 In order to play, your browser needs to manage a StarkNet wallet that will allow you to sign transactions.
             </p>
-            <a className={styles.warning} href="https://chrome.google.com/webstore/detail/argent-x-starknet-wallet/dlcobpjiigpikoobohmabehhmhfoodbb" target="_blank" rel="noreferrer" >
-                <svg className={styles.warning_icon} fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
-                <p className={styles.warning_text}>Install Argent X Wallet on Chrome</p>
+            <a className={styles.button} href="https://chrome.google.com/webstore/detail/argent-x-starknet-wallet/dlcobpjiigpikoobohmabehhmhfoodbb" target="_blank" rel="noreferrer" >
+                <svg className={styles.button_icon} fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
+                <p className={styles.button_text}>Install Argent X Wallet on Chrome</p>
             </a>
         </div>
     );
