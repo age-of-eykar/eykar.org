a996f2f348d53d086ad17e4bba1dc11b98b28efa
Thomas Marchand <thomas.marchand@tuta.io>
2022-05-20 11:03:39 +0200

    make zoom cool

--- components/map/events/zoom.js
+++ components/map/events/zoom.js
@@ -17,16 +17,24 @@ export default class ZoomControler {
         const change = event.deltaY / 1000;
         this.scale.current / 256;
 
-        /*
-                const side1 = this.selector.cursor[0] - this.center.current.x;
-                const side2 = this.selector.cursor[1] - this.center.current.y;
-                const dist = Math.sqrt(side1*side1 + side2*side2)/181;
-        
-                this.center.current = { x: this.center.current.x + side1/100,
-                y: this.center.current.y + side2/100 };
-                console.log(this.center.current)
-        */
-        this.setScale(Math.min(Math.max(this.scale.current + change * this.scale.current, 1), 256))
+        const vx = this.selector.cursor[0] - this.center.current.x;
+        const vy = this.selector.cursor[1] - this.center.current.y;
+        const norm = Math.sqrt(vx * vx + vy * vy);
+        const factor = (change < 0 ? 1 : -1) * norm / 13.3;
+
+        const nextScale = this.scale.current + change * this.scale.current;
+        if (nextScale < 1) {
+            this.setScale(1);
+        } else if (nextScale > 256) {
+            this.setScale(256);
+        } else {
+            this.setScale(nextScale)
+            this.center.current = {
+                x: this.center.current.x + factor * vx / norm,
+                y: this.center.current.y + factor * vy / norm
+            }
+        }
+
         this.selector.updateSelect();
     }
 
