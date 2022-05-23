58a5dcec7bbe5b2f54be1023918745cf349f1718
Thomas Marchand <thomas.marchand@tuta.io>
2022-05-23 17:34:06 +0200

    add close button

--- pages/explore.js
+++ pages/explore.js
@@ -10,7 +10,7 @@ export default function Explore() {
   return (
     <>
       <Header />
-      <div>{selected ? <Selected x={selected[0]} y={selected[1]} /> : undefined}</div>
+      <div>{selected ? <Selected x={selected[0]} y={selected[1]} setClicked={setClicked} /> : undefined}</div>
       <MapCanvas onPlotClick={(x, y) => {
         setClicked((currentState) => {
           return (currentState && currentState[0] === x && currentState[1] === y)
