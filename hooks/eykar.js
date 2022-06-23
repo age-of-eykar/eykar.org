aa7094b176999badf04de36679a7a74c41ebb53e
Thomas Marchand <thomas.marchand@tuta.io>
2022-06-23 13:04:25 +0200

    feat: update libs

--- hooks/eykar.js
+++ hooks/eykar.js
@@ -5,6 +5,6 @@ import EykarAbi from '../abi/eykar.json'
 export function useEykarContract() {
   return useContract({
     abi: EykarAbi,
-    address: '0x03d628b219eba75ab5400f656c7b59ec7c3fcf1073bf8a471257fc6463b8edca',
+    address: '0x036e5361579499ccbd1b61f9bec6f964698659e02d9fa578bd1fb4617175f030',
   })
 }
