6e67004c74ae414f7af6315efe54fce6a63fe5b8
Thomas Marchand <thomas.marchand@tuta.io>
2022-07-27 15:31:27 +0200

    fix numbers

--- hooks/eykar.js
+++ hooks/eykar.js
@@ -5,6 +5,6 @@ import EykarAbi from '../abi/eykar.json'
 export function useEykarContract() {
   return useContract({
     abi: EykarAbi,
-    address: '0x07000da21052c2ea6db40b56469f1f730a22cf9ef9c7251b752d0473373da3e7',
+    address: '0x005e9dccdd3bac864d60e5299a86106799396073ffbaa4adb38c53841dac1273',
   })
 }
--- utils/felt.js
+++ utils/felt.js
@@ -13,7 +13,8 @@ export function stringToFelt(str) {
 
 export function toNegativeNumber(felt) {
     const added = felt.sub(P);
-    return (added.abs() < felt.abs())
+    console.log(added.abs().toString(), felt.abs().toString())
+    return (added.abs().cmp(felt.abs())) == -1
         ? added
         : felt;
 }
