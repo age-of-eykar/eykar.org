d2632d3c0712de6830927f8b56a8ed73bef3b4ff
Thomas Marchand <thomas.marchand@tuta.io>
2022-08-18 18:47:57 +0200

    add scroll for computers

--- components/discover/slider.js
+++ components/discover/slider.js
@@ -1,9 +1,10 @@
 import styles from '../../styles/components/Slider.module.css'
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function Slider({ pages, id, setPageId, setDirection }) {
 
     const points = [];
+    const [movePercentage, setMovePercentage] = useState(0.0);
 
     useEffect(() => {
         if (typeof window === 'undefined')
@@ -44,6 +45,43 @@ function Slider({ pages, id, setPageId, setDirection }) {
 
         window.addEventListener('touchstart', handleTouchStart, false);
         window.addEventListener('touchmove', handleTouchMove, false);
+
+
+        let scrolled = 0;
+        let lastOffset = null;
+        function onScroll(event) {
+            if (event.offsetY === lastOffset) {
+                scrolled += event.deltaY;
+                let newId;
+                if (scrolled == 0)
+                    newId = -1;
+                else if (scrolled > 0)
+                    newId = (id + 1) % pages.length;
+                else
+                    newId = (id + pages.length - 1) % pages.length;
+                if (scrolled > 150) {
+                    setPageId(newId);
+                    setDirection(-1)
+                    scrolled = 0;
+                    window.scrollTo(0, 0);
+                } else if (scrolled < -150) {
+                    setPageId(newId);
+                    setDirection(1)
+                    scrolled = 0;
+                }
+            } else {
+                scrolled = 0;
+            }
+            setMovePercentage(Math.abs(scrolled) / 150)
+            lastOffset = event.offsetY;
+        }
+        window.addEventListener("wheel", onScroll);
+
+        return () => {
+            window.removeEventListener("wheel", onScroll);
+            window.removeEventListener('touchstart', handleTouchStart);
+            window.removeEventListener('touchmove', handleTouchMove);
+        }
     }, [id, pages]);
 
     for (let i = 0; i < pages.length; i++) {
@@ -51,6 +89,7 @@ function Slider({ pages, id, setPageId, setDirection }) {
             <div
                 key={i}
                 onClick={i === id ? () => { } : () => setPageId(i)}
+                style={i === id ? { "background-color": "rgba(227, 227, 227, " + (1 - movePercentage) + ")" } : {}}
                 className={i === id ? styles.big_point : styles.small_point}>
                 <span className={styles.title}>{pages[i]}</span>
             </div>)
