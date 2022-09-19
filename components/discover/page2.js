bb265c23c5b0659b7053fd5f4416f678a6a6ba79
Thomas Marchand <thomas.marchand@tuta.io>
2022-09-19 16:52:59 +0200

    add merchant and improve margins

--- components/discover/page2.js
+++ components/discover/page2.js
@@ -14,12 +14,6 @@ function Page2({ direction }) {
     return (
         <div className={[styles.page1, directionStyle].join(" ")}>
             <div className={styles.cards}>
-                <div className={[styles.vertical_card, styles.bg_merchant].join(" ")}>
-                    <div className={styles.vertical_content}>
-                        <h1>Merchants</h1>
-                        <p>Merchants gain prestige by trading. They establish trade routes and go in search of the most sought-after resources from other kingdoms.</p>
-                    </div>
-                </div>
 
                 <div className={[styles.vertical_card, styles.bg_diplomat].join(" ")}>
                     <div className={styles.vertical_content}>
@@ -34,6 +28,14 @@ function Page2({ direction }) {
                         <p>Warlords gain prestige by destroying other empires. They can put violence in the service of the wealthy or stand on the side of good and defend the just causes.</p>
                     </div>
                 </div>
+
+                <div className={[styles.vertical_card, styles.bg_merchant].join(" ")}>
+                    <div className={styles.vertical_content}>
+                        <h1>Merchants</h1>
+                        <p>Merchants gain prestige by trading. They establish trade routes and go in search of the most sought-after resources from other kingdoms.</p>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
