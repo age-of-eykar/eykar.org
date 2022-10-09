4b1d2e3a1b35059f0ac59c2e00aee27668b5df19
Thomas Marchand <thomas.marchand@tuta.io>
2022-10-09 22:37:57 +0200

    feat: update page color

--- pages/_document.js
+++ pages/_document.js
@@ -8,13 +8,13 @@ export default function Document() {
 
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
-            <meta name="theme-color" content="#cdc6aa" />
+            <meta name="theme-color" content="#282828" />
             <meta property="og:site_name" content="A decentralizied strategy game" />
             <meta property="og:title" content="Age of Eykar ⚔️" />
             <meta property="og:type" content="website" />
             <meta property="og:description" content="An infinite, unpredictable world driven by a decentralized smartcontract. Expand your empire and conquer the world." />
-            <meta property="og:url" content="%PUBLIC_URL%" />
-            <meta property="og:image" content="%PUBLIC_URL%/logo.svg" />
+            <meta property="og:url" content="https://eykar.org" />
+            <meta property="og:image" content="/logo.svg" />
             <meta name="description" content="An infinite, unpredictable world driven by a decentralized smartcontract" />
 
             <link rel="icon" href="/favicon.ico" />
